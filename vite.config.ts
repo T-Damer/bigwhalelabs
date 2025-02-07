fda32ad7d7670489b267316d66cd155fa5038982
T_DaMER <dpancov64@gmail.com>
2025-02-07 17:12:55 +0300

    Correct base

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { visualizer } from 'rollup-plugin-visualizer'
 
 export default defineConfig({
   plugins: [preact(), tsconfigPaths()],
-  base: 'bigwhalelabs',
+  base: '/bigwhalelabs/',
   build: {
     rollupOptions: {
       plugins: [
